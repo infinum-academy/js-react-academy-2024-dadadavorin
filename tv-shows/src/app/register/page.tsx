58da7bb351089d1dd4a6cc8036d75dc6718121fd
Davorin Prislin <davorin.prislin@infinum.hr>
2024-07-14 17:46:13 +0200

    Added login and register links

--- tv-shows/src/app/register/page.tsx
+++ tv-shows/src/app/register/page.tsx
@@ -1,15 +1,25 @@
 "use client";
-import { Box, Container } from "@chakra-ui/react";
+import { Box, Button, Container } from "@chakra-ui/react";
 import { RegisterForm } from "@/components/features/auth/RegisterForm/RegisterForm";
-import { AuthRedirect } from "@/components/shared/AuthRedirect/AuthRedirect";
+import NextLink from "next/link";
 
 export default function RegisterPage() {
   return (
     <>
-      <AuthRedirect to="/login" condition="loggedOut" />
       <Box bg="gray.200" paddingBlock="10" height="80vh">
         <Container>
           <RegisterForm />
+          <Box mt="10" color="white.100" textAlign="center">
+            Already have an account?{" "}
+            <Button
+              as={NextLink}
+              href="/login"
+              colorScheme="yellow"
+              variant="link"
+            >
+              Login
+            </Button>
+          </Box>
         </Container>
       </Box>
     </>
--- tv-shows/src/components/features/auth/LoginForm/LoginForm.tsx
+++ tv-shows/src/components/features/auth/LoginForm/LoginForm.tsx
@@ -8,6 +8,7 @@ import {
   Input,
   chakra,
   Text,
+  Box,
 } from "@chakra-ui/react";
 import { useForm } from "react-hook-form";
 import useSWRMutation from "swr/mutation";
@@ -15,6 +16,7 @@ import { mutator } from "@/fetchers/mutators";
 import { swrKeys } from "@/fetchers/swrKeys";
 import useSWR from "swr";
 import { fetcher } from "@/fetchers/fetcher";
+import NextLink from "next/link";
 
 interface ILoginFormInputs {
   email: string;
@@ -22,7 +24,11 @@ interface ILoginFormInputs {
 }
 
 export const LoginForm = () => {
-  const { register, handleSubmit } = useForm<ILoginFormInputs>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ILoginFormInputs>();
   const { mutate } = useSWR(swrKeys.me, fetcher);
   const { trigger } = useSWRMutation(swrKeys.sign_in, mutator, {
     onSuccess: (data) => {
@@ -46,30 +52,47 @@ export const LoginForm = () => {
         gap={3}
         onSubmit={handleSubmit(onLogin)}
       >
-        <FormControl isRequired={true}>
+        <FormControl isRequired={true} isInvalid={!!errors.email}>
           <FormLabel>Email</FormLabel>
           <Input
-            {...register("email")}
-            required
+            {...register("email", { required: "Email is required" })}
             type="email"
             bg="white.100"
             color="black"
-          ></Input>
+          />
+          {errors.email && <Text color="red.500">{errors.email.message}</Text>}
         </FormControl>
-        <FormControl isRequired={true}>
+        <FormControl isRequired={true} isInvalid={!!errors.password}>
           <FormLabel>Password</FormLabel>
           <Input
-            {...register("password")}
+            {...register("password", {
+              required: "Password is required",
+            })}
             type="password"
-            required
             bg="white.100"
             color="black"
-          ></Input>
+          />
+          {errors.password && (
+            <Text color="red.500">{errors.password.message}</Text>
+          )}
         </FormControl>
         <Button type="submit" colorScheme="yellow" size="lg" marginTop="5">
           Login
         </Button>
       </chakra.form>
+      <Box mt="10">
+        <Text>
+          Don&apos;t have an account?{" "}
+          <Button
+            as={NextLink}
+            href="/register"
+            colorScheme="yellow"
+            variant="link"
+          >
+            Register
+          </Button>
+        </Text>
+      </Box>
     </Flex>
   );
 };
